490e3d252dcdb00721902c7efb6c61274f1de3d6
Md. Asif Joardar <mdasifjoardar@gmail.com>
2024-05-15 12:03:50 +0600

    add item load more

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,31 @@
 import Item from "./Item";
 import "../css/styles.css"
+import {useState} from "react";
+
+const LOAD_ITEM = 2;
 
 function TodoList({todos, setTodos}) {
+    const [displayedData, setDisplayedData] = useState(todos);
+    const [visibleItemCount, setVisibleItemCount] = useState(LOAD_ITEM);
+
+    function handleLoadMore() {
+        setVisibleItemCount(prevState => prevState + LOAD_ITEM);
+    }
+
     return (
-        <ol className="todo_list">
-            {todos && todos.length > 0 ? (
-                todos?.map((item, index) => (
-                    <Item key={index} item={item} todos={todos} setTodos={setTodos} />
-                ))
-            ) : (
-                <p>Seems lonely in here, what are you up to?</p>
-            )}
-        </ol>
+        <>
+            <ol className="todo_list">
+                {todos && todos.length > 0 ? (
+                    todos.slice(0, Math.min(visibleItemCount, todos.length)).map((item, index) => (
+                        <Item key={index} item={item} todos={todos} setTodos={setTodos}/>
+                    ))
+                ) : (
+                    <p>Seems lonely in here, what are you up to?</p>
+                )}
+            </ol>
+            {todos.length > visibleItemCount && <button className="loadMoreButton" onClick={handleLoadMore}>Load More</button>}
+        </>
     );
 }
+
 export default TodoList;
